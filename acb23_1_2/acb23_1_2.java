5e68dc23e4d2ec1936704f65888367934841e3e1
artyom.kolesnikov <artyom.kolesnikov@gmail.com>
2017-04-02 08:31:08 +0000

    1_2_3

--- acb23_1_2/acb23_1_2.java
+++ acb23_1_2/acb23_1_2.java
@@ -2,15 +2,14 @@ class acb_1_2{
 
 	public static void main (String[] args){
 
-		double a = 1;
-		double b = 2;
-		double c = 3;
+		double a = 7;
+		double b = 10;
+		double c = 5;
 
 		double area = 0;
 		double p = 0;
 		
 		p = (a + b + c)/2;
- System.out.println(area);        
 		area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 		
 		System.out.print("Triangle area is:");
