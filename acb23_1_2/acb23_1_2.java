52f5457d1e22be4ae2c5977fe2416e22d9fbda58
aikolesnikov <artyom.kolesnikov@gmail.com>
2017-04-02 11:45:37 +0300

    Update acb23_1_2.java

--- acb23_1_2/acb23_1_2.java
+++ acb23_1_2/acb23_1_2.java
@@ -1,22 +1,33 @@
-class acb_1_2{
+    public static boolean isValidTriangle(double a, double b, double c) {
+        return ((a + b > c) && (b + c > a) && (c + a > b));
+    }
 
-	public static void main (String[] args){
+    public static double evaluatePerimeter(double a, double b, double c) {
+        return (a + b + c);
+    }
 
-		double a = 7;
-		double b = 10;
-		double c = 5;
+    public static void main(String[] args) {
+        // write your code here
 
-		double area = 0;
-		double p = 0;
-		
-		p = (a + b + c)/2;
-		area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-		
-		System.out.print("Triangle area is:");
-		System.out.println(area);
-		
-	}
+        double a = 7;
+        double b = 10;
+        double c = 5;
 
+        double perim = 0;
+        double area = 0;
 
+        if (isValidTriangle(a, b, c)) {
+            perim = evaluatePerimeter(a, b, c) / 2;
+            System.out.println(area);
+            area = Math.sqrt(perim * (perim - a) * (perim - b) * (perim - c));
 
-}
+            System.out.print("Triangle area is:");
+            System.out.println(area);
+
+            System.out.println("Formatted:");
+            System.out.printf("a = %.2f, b = %.2f, c = %.2f\n", a, b, c);
+            System.out.printf("Area is %.2f\n", area);
+
+        } else
+        System.out.println("no way");
+    }
