055e22fbdc29474c7c045bc25fab9cb76a5c89cb
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-04-04 19:50:16 +0300

    hw_1

---
+++ acb23_1_1/acb23_1_01/Task_01.java
@@ -0,0 +1,12 @@
+package acb23_1_01;
+
+/**
+ * Created by artyom.kolesnikov on 4/4/2017.
+ */
+public class Task_01 {
+
+    public static void main(String[] args) {
+        System.out.println("hw1_1");
+    }
+
+}
