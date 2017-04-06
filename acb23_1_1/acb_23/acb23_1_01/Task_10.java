19673f3aa8848fffc0c0bfb93fb3cc9ec39d59b7
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-04-06 17:17:23 +0300

    hw_1

--- acb23_1_1/acb_23/acb23_1_01/Task_10.java
+++ acb23_1_1/acb_23/acb23_1_01/Task_10.java
@@ -1,5 +1,6 @@
 package acb_23.acb23_1_01;
 
+import java.util.Scanner;
 import static acb_23.utilities.Input.inputInt;
 
 /**
@@ -10,24 +11,24 @@ public class Task_10 {
     public static void main(String[] args) {
 
         int i = 0, t = 0;
+
         i = inputInt("Input i:");
         t = i;
-        String bnr = "";
+        String bnrS = "";
 
         while (t != 0)  {
-            // System.out.println(t%2==0 ? 0 : 1);
-            if (t%2 == 0)
-                bnr = "0" + bnr;
-            else
-                bnr = "1" + bnr;
+            bnrS = (t%2 == 0) ? "0" + bnrS : "1" + bnrS;
             t = t / 2;
         }
 
-        System.out.printf("i=%d", i);
-        System.out.println();
-        System.out.println(bnr);
+        System.out.println(bnrS);
         System.out.print("Проверка: ");
         System.out.println(Integer.toBinaryString(i));
         //System.out.println(Integer.parseInt(Integer.toBinaryString(i), 2));
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите двоичное число: ");
+        in.next(bnrS);
+        System.out.println(Integer.parseInt(bnrS, 2));
     }
 }
