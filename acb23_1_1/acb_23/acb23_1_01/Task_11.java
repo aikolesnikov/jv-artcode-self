1e256b0cd6c5e00dc48179c5ca1fac02423ea8b0
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-04-07 12:26:38 +0300

    hw_1

---
+++ acb23_1_1/acb_23/acb23_1_01/Task_11.java
@@ -0,0 +1,28 @@
+package acb_23.acb23_1_01;
+
+import static acb_23.utilities.Input.inputInt;
+
+/**
+ * 5.2. Вычислить факториал числа.
+ */
+public class Task_11 {
+
+    private static int Fact(int i) {
+        long f = 1;
+
+        if (i < 0) {
+            return 0;
+        } else if (i == 0) {
+            return 1;
+        } else {
+            return i * Fact(i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        i = inputInt("Input i:");
+
+        System.out.println(Fact(i));
+    }
+}
