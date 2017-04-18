7d333c15ba0e7968405b7261645d02f2477cf90c
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-04-18 19:41:12 +0300

    of1

--- acb23_1/week_2/Task_2_01.java
+++ acb23_1/week_2/Task_2_01.java
@@ -1,6 +1,5 @@
 package week_2;
 
-
 import utilities.Input;
 import utilities.Output;
 
@@ -11,10 +10,29 @@ public class Task_2_01 {
 
     public static void main(String[] args) {
 
-        int[] ar = Input.generateIntAr(10, 0, 10);
-        Output.printArray(ar);
+        int size = Input.inputInt("Array size?: ");
 
+        if (size > 0) {
+            int rangeFrom = Input.inputInt("Values from?: ");
+            int rangeTo = Input.inputInt("Values to?: ");
 
+            int[] ar = Input.generateIntAr(size, rangeFrom, rangeTo);
+            Output.printArray(ar);
+
+            int min = ar[0];
+            int max = ar[0];
+
+            for (int i = 1; i < ar.length; i++) {
+                if (ar[i] < min) min = ar[i];
+                if (ar[i] > max) max = ar[i];
+            }
+
+            System.out.println("min = " + min);
+            System.out.println("max = " + max);
+        }
+        else {
+            System.out.println("Size must be >0!");
+        }
     }
 
 }
