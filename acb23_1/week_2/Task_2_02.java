692fd4d2d54f2f43e5db9147da06871d9cfe1d17
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-04-22 11:52:50 +0300

    v3

--- acb23_1/week_2/Task_2_02.java
+++ acb23_1/week_2/Task_2_02.java
@@ -6,7 +6,7 @@ import utilities.Output;
 /**
  * Найти минимальное и максимальное значения в массиве и вывести их на консоль
  */
-public class Task_2_01 {
+public class Task_2_02 {
 
     public static void main(String[] args) {
 
@@ -17,20 +17,39 @@ public class Task_2_01 {
             int rangeTo = Input.inputInt("Values to?: ");
 
             int[] ar = Input.generateIntAr(size, rangeFrom, rangeTo);
+
+            System.out.println("Array before changing:");
             Output.printArray(ar);
 
             int min = ar[0];
+            int minPos = 0;
             int max = ar[0];
+            int maxPos = 0;
 
             for (int i = 1; i < ar.length; i++) {
-                if (ar[i] < min) min = ar[i];
-                if (ar[i] > max) max = ar[i];
+                if (ar[i] < min) {
+                    min = ar[i];
+                    minPos = i;
+                }
+                if (ar[i] > max) {
+                    max = ar[i];
+                    maxPos = i;
+                }
             }
 
             System.out.println("min = " + min);
             System.out.println("max = " + max);
-        }
-        else {
+
+            if (!(minPos == maxPos)) {
+                int tmpArVal = ar[minPos];
+                ar[minPos] = ar[maxPos];
+                ar[maxPos] = tmpArVal;
+            }
+
+            System.out.println("Array after changing:");
+            Output.printArray(ar);
+
+        } else {
             System.out.println("Size must be > 0 !");
         }
     }
