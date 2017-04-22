10190d0d3a25aba6dc28d5c1f320bf23708c484b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-04-22 12:02:46 +0300

    v3

--- acb23_1/week_2/Task_2_03.java
+++ acb23_1/week_2/Task_2_03.java
@@ -10,27 +10,29 @@ public class Task_2_03 {
 
     public static void main(String[] args) {
 
-        int size = Input.inputInt("Array size?: ");
+        int size = Input.inputInt("Array1 size?: ");
 
         if (size > 0) {
             int rangeFrom = Input.inputInt("Values from?: ");
             int rangeTo = Input.inputInt("Values to?: ");
 
             int[] ar = Input.generateIntAr(size, rangeFrom, rangeTo);
+            int[] ar2 = new int[size];
+            System.out.println("First array:");
             Output.printArray(ar);
+            System.out.println("Second array:");
+            Output.printArray(ar2);
 
-            int min = ar[0];
-            int max = ar[0];
+            System.arraycopy(ar, 1, ar2, 1, ar.length - 1);
 
+            /*
             for (int i = 1; i < ar.length; i++) {
-                if (ar[i] < min) min = ar[i];
-                if (ar[i] > max) max = ar[i];
+                ar2[i] = ar[i];
             }
-
-            System.out.println("min = " + min);
-            System.out.println("max = " + max);
-        }
-        else {
+            */
+            System.out.println("Second array after copying:");
+            Output.printArray(ar2);
+        } else {
             System.out.println("Size must be > 0 !");
         }
     }
