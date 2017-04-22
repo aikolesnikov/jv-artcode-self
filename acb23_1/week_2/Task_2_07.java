54b8a9a524521783f7fe0b87cc8636d22257353c
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-04-22 21:06:02 +0300

    v3

--- acb23_1/week_2/Task_2_07.java
+++ acb23_1/week_2/Task_2_07.java
@@ -1,8 +1,10 @@
 package week_2;
 
-import utilities.Input;
 import utilities.Output;
 import utilities.Process;
+
+import static utilities.Input.generateIntAr;
+import static utilities.Input.inputInt;
 
 /**
  * Вывести в консоль элементы той половины одномерного массива
@@ -13,18 +15,43 @@ public class Task_2_07 {
 
     public static void main(String[] args) {
 
-        int size = Input.inputInt("Array size?: ");
+        int size = inputInt("Array size?: ");
         if (size == 0) {
             System.out.println("Size must be > 0 !");
             return;
         }
 
-        int[] ar = Input.generateIntAr(size,
-                Input.inputInt("Values from?: "), Input.inputInt("Values to?: "));
+        int[] ar = generateIntAr(size, inputInt("Values from?: "), inputInt("Values to?: "));
         Output.printArray("Array:", ar);
 
+        int sumArPart1 = 0;
+        int sumArPart2 = 0;
+        int arMiddle = ar.length / 2;
 
-        System.out.println("Average is: " + ((double) Process.sumIntAr(ar)) / ar.length);
+        System.out.print("Left part: ");
+        for (int i = 0; i < arMiddle + ar.length % 2; i++) {
+            System.out.print(ar[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Right part: ");
+        for (int i = 0; i < arMiddle + ar.length % 2; i++) {
+            System.out.print(ar[arMiddle + i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arMiddle; i++) {
+            sumArPart1 += ar[i];
+            sumArPart2 += ar[arMiddle + i];
+        }
+
+        System.out.println(sumArPart1 + " " + sumArPart2);
+        if (sumArPart1 > sumArPart2) {
+            System.out.println("Left part is bigger:");
+        }
+
+        // System.out.println("Average is: " + ((double) Process.sumIntAr(ar)) / ar.length);
+
     }
 
 }
