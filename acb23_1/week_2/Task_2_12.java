cdd73b019426416f76675a6fdaf64be822255fc7
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-04-23 12:28:51 +0300

    v3

--- acb23_1/week_2/Task_2_12.java
+++ acb23_1/week_2/Task_2_12.java
@@ -8,11 +8,11 @@ import utilities.Input;
 public class Task_2_12 {
 
     private static boolean isSimple(int n) {
-        if (n == 0 || n == 1) return true;
+        if (n == 0 || n == 1 || n == 2 || n == 3) return true;
 
         int i = 2;
 
-        while (i < n / 2) {
+        while (i <= n / 2) {
             if (n % i++ == 0) {
                 return false;
             }
@@ -24,7 +24,6 @@ public class Task_2_12 {
     public static void main(String[] args) {
 
         int n = Input.inputInt("Input number:");
-
         System.out.println(isSimple(n));
     }
 
