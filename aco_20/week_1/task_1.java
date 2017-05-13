0b1af18f01bf67ff1eaa636af302f0fb8cd664c2
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-05-13 16:04:23 +0300

    v3

---
+++ aco_20/week_1/task_1.java
@@ -0,0 +1,22 @@
+/**
+ *  Created by kaim on 5/13/17.
+ */
+public class task_1 {
+
+    public static class Student{
+        String name;
+        String surname;
+        int age;
+
+        Student(){}
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.name = "Ya";
+
+        System.out.println(s1.name);
+
+    }
+}
--- aco_20/week_1/w.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kaim on 5/13/17.
- */
-public class w {
-
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
