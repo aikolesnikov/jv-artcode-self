21ee81fecb0e48959a8935a5fccc00958200d83e
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-05-19 22:45:22 +0300

    withMenu

---
+++ aco_20/week_1/books/Book.java
@@ -0,0 +1,12 @@
+package books;
+
+/**
+ * abstract book
+ */
+public abstract class Book {
+    String name;
+    String author;
+
+    abstract boolean print();
+    abstract int publish();
+}
---
+++ aco_20/week_1/books/IEditable.java
@@ -0,0 +1,10 @@
+package books;
+
+/**
+ * interface for books
+ */
+public interface IEditable {
+    String PublishHouse();
+    int PublishYear();
+    int PublishQty();
+}
