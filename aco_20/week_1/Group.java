baf5292c1fc8cb32f9fdb59ae96bf81ab3748b75
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-05-19 17:57:32 +0300

    ++ logging

--- aco_20/week_1/Group.java
+++ aco_20/week_1/Group.java
@@ -1,7 +1,7 @@
 /**
  *  Created by kaim on 5/14/17.
  */
-    class Group {
+class Group {
 
     private int groupNumber;
     private int groupSize;
@@ -10,12 +10,15 @@
     private void setGroupNumber(int groupNumber) {
         this.groupNumber = groupNumber;
     }
+
     public int getGroupNumber() {
         return groupNumber;
     }
+
     private void setGroupSize(int groupSize) {
         this.groupSize = groupSize;
     }
+
     public int getGroupSize() {
         return groupSize;
     }
@@ -25,6 +28,7 @@
         this.setGroupSize(students.length);
         this.students = students;
     }
+
     Group(int num, int groupSize) {
         this.setGroupNumber(num);
         this.setGroupSize(groupSize);
@@ -38,19 +42,20 @@
         for (int i = 0; i < this.groupSize; i++) {
             if (this.students[i] == null) {
                 this.groupSize++;
-                this.students[i]=student;
+                this.students[i] = student;
                 return true;
             }
         }
 
         return false;
     }
+
     String Print() {
 
         String info = "";
 
         for (int i = 0; i < this.groupSize; i++) {
-            if (!(students[i] == null)){
+            if (!(students[i] == null)) {
                 info += students[i].Print();
                 info += ";\n ";
             }
