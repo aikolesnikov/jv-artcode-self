f011a2288cdb33b9a232607773f44d2115442c1d
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-05-23 20:09:31 +0300

    new structure

--- src/main/java/datastructures/MyArrayList.java
+++ src/main/java/datastructures/MyArrayList.java
@@ -5,6 +5,12 @@ package datastructures;
  */
 public class MyArrayList implements MyList {
 
+/*
+    //  with an initial capacity of ten.  ArrayList();                                // Constructs an empty list with an initial capacity of ten.
+    ArrayList(Object o);       // Constructs a list containing the elements of the specified collection, in the order they are returned by the collection's iterator.
+    ArrayList(int initialCapacity);             // Constructs an empty list with the specified initial capacity.
+*/
+
     @Override
     public boolean add(Object o) {
         return false;
--- src/main/java/datastructures/MyCollection.java
+++ src/main/java/datastructures/MyCollection.java
@@ -1,32 +1,25 @@
 package datastructures;
 
-import _template.Entity;
-
-import java.util.Iterator;
-import java.util.Objects;
-
 /**
- *  Created by kaim on 5/21/17.
+ *  base template interface for MyDataStructures
  */
 interface MyCollection{
 
-   boolean	add(Object o); // Ensures that this collection contains the specified element (optional operation).
-   // void	clear(); //     Removes all of the elements from this collection (optional operation).
+    boolean	add(Object oAdd);               // Ensures that this collection contains the specified element (optional operation).
+    void	clear();                        // Removes all of the elements from this collection (optional operation).
+    boolean	contains(Object oContains);     // Returns true if this collection contains the specified element.
+    // boolean	equals(Object oEquals);     // Compares the specified object with this collection for equality.
+    boolean	remove(Object oRemove);         // Removes a single instance of the specified element from this collection, if it is present (optional operation).
+    boolean	isEmpty();                      // Returns true if this collection contains no elements.
+    int	size();                             // Returns the number of elements in this collection.
+    Object[] toArray();                     // Returns an array containing all of the elements in this collection.
 
-    /*
-
-
-    boolean	contains(Object o);     Returns true if this collection contains the specified element.
+/*
     boolean	containsAll(Collection<?> c);       Returns true if this collection contains all of the elements in the specified collection.
-    boolean	equals(Object o);       Compares the specified object with this collection for equality.
     int	hashCode();     Returns the hash code value for this collection.
-    boolean	isEmpty();      Returns true if this collection contains no elements.
     Iterator<E>	iterator();     Returns an iterator over the elements in this collection.
-    boolean	remove(Object o);   Removes a single instance of the specified element from this collection, if it is present (optional operation).
     boolean	removeAll(Collection<?> c);     Removes all of this collection's elements that are also contained in the specified collection (optional operation).
     boolean	retainAll(Collection<?> c);     Retains only the elements in this collection that are contained in the specified collection (optional operation).
-    int	size();     Returns the number of elements in this collection.
-    Object[] toArray();         Returns an array containing all of the elements in this collection.
     <T> T[]	toArray(T[] a);     Returns an array containing all of the elements in this collection; the runtime type of the returned array is that of the specified array.
 */
 
