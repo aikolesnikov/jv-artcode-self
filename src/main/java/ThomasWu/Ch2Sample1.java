64d44925ce4b8d6eb97a14a169c841ca379f448d
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-05-27 15:12:26 +0300

    3rd

---
+++ src/main/java/ThomasWu/Ch2Sample1.java
@@ -0,0 +1,18 @@
+package ThomasWu;
+
+/**
+ *  book
+ */
+
+import javax.swing.*;
+
+public class Ch2Sample1 {
+
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+    }
+}
