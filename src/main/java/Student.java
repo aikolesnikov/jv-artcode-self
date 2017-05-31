95d5afc6eeb5ea9b8a74dd441ed58db777096572
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-05-31 19:42:10 +0300

    isecond run

---
+++ src/main/java/Student.java
@@ -0,0 +1,23 @@
+/**
+ * student
+ */
+public class Student {
+    private String name;
+    private String surname;
+    private int age;
+
+    Student() {
+    }
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    Student (String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+}
+
+
