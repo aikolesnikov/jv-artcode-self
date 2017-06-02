b445ee34de0fec92946b9734e3df47ff212fd875
Artyom Kolesnikov <artyom.kolesnikov@worldapp.com>
2017-06-02 12:38:54 +0300

    isecond run

---
+++ src/main/java/datastructures/MyList.java
@@ -0,0 +1,14 @@
+package datastructures;
+
+/**
+ * base template interface for MyList
+ */
+interface MyList extends MyCollection {
+    Object get(int index);                          //  Returns the element at the specified position in this list.
+    void resize(int newSize);
+    int indexOf(Object o);                          //  Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+    // int lastIndexOf(Object o);                   //  Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element.
+    boolean remove(int index);                      //  Removes the element at the specified position in this list (optional operation).
+    boolean set(int index, Object o);               //  Replaces the element at the specified position in this list with the specified element (optional operation).
+    // Object subList(int fromIndex, int toIndex);     //  Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive.
+}
